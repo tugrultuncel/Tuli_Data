e4461e2fb8d4c9b419838f516df5c33ae3cb55d0
tugrul tuncel <tugrultuncel95@gmail.com>
2026-01-29 17:35:45 +0100

    suggestion extractor set for real estate keywords

--- suggestion_extractor.py
+++ suggestion_extractor.py
@@ -2,12 +2,12 @@ from pytrends.request import TrendReq
 
 pytrends = TrendReq(hl='pl-PL', tz=360)
 kw_list = [
-#    "mieszkania",
-#    "wynajem",
-#    "apartment",
-#    "dom",
+    "mieszkania",
+    "wynajem",
+    "apartment",
+    "dom",
     "kavalerka",
-#    "pokoje"
+    "pokoje"
 ]
 for kw in kw_list:
     print(f'\nsuggestions for "{kw}":')
